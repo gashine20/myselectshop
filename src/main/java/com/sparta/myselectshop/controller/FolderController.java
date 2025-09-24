b380fb1f5f737f9a4ca78935a2350626e93be3a6
gahyeon <1000ji1@naver.com>
2025-09-24 23:19:49 +0900

    :sparkles: 관심 상품에 폴더 추가 및 폴더 전체 조회 구현

--- src/main/java/com/sparta/myselectshop/controller/FolderController.java
+++ src/main/java/com/sparta/myselectshop/controller/FolderController.java
@@ -1,11 +1,13 @@
 package com.sparta.myselectshop.controller;
 
+import com.sparta.myselectshop.dto.FolderReponseDto;
 import com.sparta.myselectshop.dto.FolderRequestDto;
 import com.sparta.myselectshop.security.UserDetailsImpl;
 import com.sparta.myselectshop.service.FolderService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +21,14 @@ public class FolderController {
     private final FolderService folderService;
 
     @PostMapping("/folders")
-    public void addFolders(@RequestBody FolderRequestDto folderRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public void addFolders(@RequestBody FolderRequestDto folderRequestDto,
+                           @AuthenticationPrincipal UserDetailsImpl userDetails) {
         List<String> folderNames = folderRequestDto.getFolderNames();
         folderService.addFolders(folderNames, userDetails.getUser());
+    }
+
+    @GetMapping("/folders")
+    public List<FolderReponseDto> getFolders(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return folderService.getFolders(userDetails.getUser());
     }
 }
